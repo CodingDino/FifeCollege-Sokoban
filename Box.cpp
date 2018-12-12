4b6cd33bfc3d496af53479cfb8b5d2029873f4f0
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-12-12 14:19:47 +0000

    Box class with blocking (no push)

---
+++ Box.cpp
@@ -0,0 +1,10 @@
+// Project Includes
+#include "Box.h"
+#include "Framework/AssetManager.h"
+
+Box::Box()
+	: GridObject()
+{
+	m_sprite.setTexture(AssetManager::GetTexture("graphics/box.png"));
+	m_blocksMovement = true;
+}
--- Level.cpp
+++ Level.cpp
@@ -4,6 +4,7 @@
 #include "Wall.h"
 #include "Storage.h"
 #include "Player.h"
+#include "Box.h"
 
 // Library Includes
 #include <iostream>
@@ -198,6 +199,13 @@ void Level::LoadLevel(int _levelToLoad)
 				player->SetLevel(this);
 				player->SetGridPosition(x, y);
 				m_contents[y][x].push_back(player);
+			}
+			else if (ch == 'B')
+			{
+				Box* box = new Box();
+				box->SetLevel(this);
+				box->SetGridPosition(x, y);
+				m_contents[y][x].push_back(box);
 			}
 			else
 			{
